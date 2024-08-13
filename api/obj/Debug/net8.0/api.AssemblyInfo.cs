4ed581f054cc6dea9c44d12b066d7291068119dd
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-08-13 04:33:44 +0300

    User Auth Token Service

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4e758f05d93041f59484904598939f06e24c11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595559ed2b74d28c14abda21e48620c794503d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
